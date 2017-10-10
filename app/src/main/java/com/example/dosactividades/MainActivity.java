01a0b038ed93a7ffb1a76696ce123b9691b29cb9
Carlos Cruz Domínguez <carloscruzdominguez97@gmail.com>
2017-10-10 12:16:20 +0200

    Añado comprobación de URL

--- app/src/main/java/com/example/dosactividades/MainActivity.java
+++ app/src/main/java/com/example/dosactividades/MainActivity.java
@@ -5,11 +5,12 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.URLUtil;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnSaludar;
     private Button btnNavegar;
@@ -22,10 +23,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnNavegar = (Button)findViewById(R.id.btnNavegar);
-        btnSaludar = (Button)findViewById(R.id.btnSaludar);
-        edtNombre = (EditText)findViewById(R.id.edtNombre);
-        edtUrl = (EditText)findViewById(R.id.edtUrl);
+        btnNavegar = (Button) findViewById(R.id.btnNavegar);
+        btnSaludar = (Button) findViewById(R.id.btnSaludar);
+        edtNombre = (EditText) findViewById(R.id.edtNombre);
+        edtUrl = (EditText) findViewById(R.id.edtUrl);
 
         btnNavegar.setOnClickListener(MainActivity.this);
         btnSaludar.setOnClickListener(MainActivity.this);
@@ -46,11 +47,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void openWebPage(String url) {
-        //Debe ser del tipo https://... sino dice que no hay navegador.
-        Uri uri = Uri.parse(url);
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
+        //Comprobamos que se trata de un https: o http:. Tambien lo podemos comprobar nosotros
+        //con url.substring y extraemos del 0 al 7 (extrae uno menos del final) para http:// y
+        //del 0 al 8 para https://
+        if (URLUtil.isHttpsUrl(url) || URLUtil.isHttpUrl(url)) {
+            //Debe ser del tipo https://... sino dice que no hay navegador.
+            Uri uri = Uri.parse(url);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            //Comprobamos que hay navegador instalado tambien.
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "No hay un navegador instalado", Toast.LENGTH_SHORT).show();
+            }
         }
         else {
             Toast.makeText(this, "No hay un navegador instalado", Toast.LENGTH_SHORT).show();
